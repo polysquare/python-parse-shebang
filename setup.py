d0ba20520b69bf561ef71255c526b34c5fd0d2be
Travis-CI <travis@travis-ci.org>
2016-03-06 23:50:15 +0000

    Bump version: 0.0.12 -> 0.0.13
    [ci skip]

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 from setuptools import find_packages, setup
 
 setup(name="parse-shebang",
-      version="0.0.12",
+      version="0.0.13",
       description="""Parse shebangs and return their components.""",
       long_description_markdown_filename="README.md",
       author="Sam Spilsbury",
